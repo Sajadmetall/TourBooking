56d84487ba0d3d45c6d9a4564b70e6dac94fbb1d
sajad Rezaiyan <sajad.rezaiyan@adesso.de>
2022-10-18 10:19:10 +0200

    test3

--- TourBooking/Startup.cs
+++ TourBooking/Startup.cs
@@ -28,6 +28,9 @@ namespace TourBooking
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDBContext dataContext)
         {
+            // changes 1
+            // changes 2
+            // changes 3
             dataContext.Database.Migrate();
             //ffffdszjdsfdfdfd
             // second change
